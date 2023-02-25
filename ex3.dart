305a9696a2ffde2843fa5d6040178880907723db
Abdulkadir Gobena Denboba <86346817+Abdulke96@users.noreply.github.com>
2023-02-25 12:47:49 +0200

    Add files via upload

---
+++ ex3.dart
@@ -0,0 +1,18 @@
+// this is string 
+void main(){
+String name = "Abdulke";
+print(name[0]);
+print(name[4]);
+for(int i  = 0; i< name.length; i++ ){
+  print(name[i]);
+}
+print(name.length);
+print(name.toLowerCase());
+print(name.toUpperCase());
+print(name.toString());
+print(name +name);
+print(2+4);
+print(4/3);
+print(2-4+8*4/5);
+print(3%2);
+}
